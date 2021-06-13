b5c010d679ef64a5efd1c728524f2794f1910155
Mehmet Emin Bozgeyik <eminbozgeyik@hotmail.com>
2021-06-13 17:48:19 +0300

    new commits

--- DevFramework.Core/DataAccess/NHibernate/NHibernateHelper.cs
+++ DevFramework.Core/DataAccess/NHibernate/NHibernateHelper.cs
@@ -11,17 +11,14 @@ namespace DevFramework.Core.DataAccess.NHibernate
     {
         private static ISessionFactory _sessionFactory;
 
-        public virtual ISessionFactory SessionFactory
-        {
+        public virtual ISessionFactory SessionFactory {
             get { return _sessionFactory ?? (_sessionFactory = InitializeFactory()); }
         }
 
         protected abstract ISessionFactory InitializeFactory();
 
-        public virtual ISession OpenSession()
-        {
-            return SessionFactory.OpenSession();
-        }
+        public virtual ISession OpenSession() { 
+            return SessionFactory.OpenSession(); }
 
         public void Dispose()
         {
